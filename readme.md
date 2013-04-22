0cf401c8fd556b8b33ba8ee912ea7808b5a6c16b
Samuel Grahn <grahnen@samuelmac.(none)>
2013-04-22 09:06:05 +0200

    Changed Readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+THIS IS A README, JUST SO YOU KNOW.
+
+This game isn't done yet. It probably wont be anytime soon. But still.
+
+It's a game nonetheless.
--- src/grahnathan/rpg/game/character/ability/Ability.java
+++ src/grahnathan/rpg/game/character/ability/Ability.java
@@ -1,9 +1,38 @@
 package grahnathan.rpg.game.character.ability;
 
-public class Ability 
+import grahnathan.rpg.game.character.Char;
+
+public abstract class Ability 
 {
-	public void Use()
-	{
-		
+	private int cooldown;
+	private int turnsSinceLastUse;
+	public Target targetType;
+	
+	enum Target{
+		Enemy,
+		Ally,
+		Area,
+		Self
 	}
+
+	public Ability(int cooldown, Target targetType)
+	{
+		this.cooldown = cooldown;
+		this.turnsSinceLastUse = 0;
+		this.targetType = targetType;
+	}
+	
+	public void OnTurnStart()
+	{
+		this.turnsSinceLastUse++;
+	}
+	
+	public boolean Available()
+	{
+		return turnsSinceLastUse >= cooldown;
+	}
+	
+	public abstract void Use();
+	public abstract void Use(Char c);
+	public abstract void Use(int x, int y);
 }
